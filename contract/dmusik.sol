2bcd81c9f2d791566c7781a7b1e42548eb4836f3
Ibukunoluwa985 <promisenap2001@gmail.com>
2022-01-22 19:28:28 +0100

    update Dapp

--- contract/dmusik.sol
+++ contract/dmusik.sol
@@ -2,15 +2,27 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 interface IERC20Token {
-  function transfer(address, uint256) external returns (bool);
-  function approve(address, uint256) external returns (bool);
-  function transferFrom(address, address, uint256) external returns (bool);
-  function totalSupply() external view returns (uint256);
-  function balanceOf(address) external view returns (uint256);
-  function allowance(address, address) external view returns (uint256);
+    function transfer(address, uint256) external returns (bool);
 
-  event Transfer(address indexed from, address indexed to, uint256 value);
-  event Approval(address indexed owner, address indexed spender, uint256 value);
+    function approve(address, uint256) external returns (bool);
+
+    function transferFrom(
+        address,
+        address,
+        uint256
+    ) external returns (bool);
+
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address) external view returns (uint256);
+
+    function allowance(address, address) external view returns (uint256);
+
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
 }
 
 library SafeMath {
@@ -20,109 +32,48 @@ library SafeMath {
 
         return c;
     }
-
-    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-        return sub(a, b, "SafeMath: subtraction overflow");
-    }
-
-    function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
-        require(b <= a, errorMessage);
-        uint256 c = a - b;
-
-        return c;
-    }
-
-    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-        if (a == 0) {
-            return 0;
-        }
-
-        uint256 c = a * b;
-        require(c / a == b, "SafeMath: multiplication overflow");
-
-        return c;
-    }
-
-    function div(uint256 a, uint256 b) internal pure returns (uint256) {
-        return div(a, b, "SafeMath: division by zero");
-    }
-
-    function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
-        require(b > 0, errorMessage);
-        uint256 c = a / b;
-        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-
-        return c;
-    }
-
-    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
-        return mod(a, b, "SafeMath: modulo by zero");
-    }
-
-    function mod(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
-        require(b != 0, errorMessage);
-        return a % b;
-    }
 }
 
-
 contract DmusikMarketplace {
+    address internal cUsdTokenAddress =
+        0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
+    using SafeMath for uint256;
 
-    address internal cUsdTokenAddress = 0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
-    using SafeMath for uint;
-
-    uint internal argumentsLength = 0;
-    // support price 1 cusd
-    uint supportPrice = 2e18;
-    uint likePrice = 1e18;
+    uint256 internal argumentsLength = 0;
+    // support price 2 cusd
+    uint256 supportPrice = 2e18;
+    uint256 likePrice = 1e18;
     address _contractOwner;
 
     struct Dmusik {
         address payable owner;
-        mapping (address => uint) likes;
-        uint likesCount;
-        uint likesSum;
-        uint support;
+        mapping(address => bool) likes;
+        uint256 likesCount;
+        uint256 support;
     }
 
-    mapping (uint => Dmusik) internal dmusik;
+    mapping(uint256 => Dmusik) internal dmusik;
 
-    constructor() {
-        _contractOwner = msg.sender;
+    function likeSong(uint256 _index) public payable {
+        bool prevLike = dmusik[_index].likes[msg.sender];
+        require(prevLike == false, "You already liked this song.");
+        
+        dmusik[_index].likesCount = dmusik[_index].likesCount.add(1);
+        dmusik[_index].likes[msg.sender] = true;
     }
-    
-    modifier onlyOwner() {
-        require(msg.sender == _contractOwner);
-        _;
-    }
 
-
-    function likeSong(uint _index, uint _like) public payable {        
-        require(
-          IERC20Token(cUsdTokenAddress).transferFrom(
-            msg.sender,
-            _contractOwner,
-            likePrice
-          ),
-          "Transfer failed."
-        );
-
-        uint prevLike = dmusik[_index].likes[msg.sender];
-        if(prevLike == 0) {
-            dmusik[_index].likesCount++;
-        }
-        dmusik[_index].likesSum += _like;
-        dmusik[_index].likes[msg.sender] = _like;
-    }
-    
-    function getLikesCount(uint _index) public view returns (uint) {
+    function getLikesCount(uint256 _index) public view returns (uint256) {
         return dmusik[_index].likesCount;
     }
 
-   function dmusikSupport() public payable {
-        payable(msg.sender);
-            require(IERC20Token(cUsdTokenAddress).transferFrom(msg.sender, _contractOwner,supportPrice),
-          "Transfer failed."
+    function dmusikSupport() public payable {
+        require(
+            IERC20Token(cUsdTokenAddress).transferFrom(
+                msg.sender,
+                _contractOwner,
+                supportPrice
+            ),
+            "Transfer failed."
         );
-	}
+    }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -4,29 +4,38 @@ const FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
-  devtool: "cheap-module-eval-source-map",
-  entry: {
-    main: path.resolve(process.cwd(), "src", "main.js")
-  },
-  output: {
-    path: path.resolve(process.cwd(), "docs"),
-    publicPath: ""
-  },
-	node: {
-   fs: "empty",
-	 net: "empty"
-	},
-  watchOptions: {
-    // ignored: /node_modules/,
-    aggregateTimeout: 300, // After seeing an edit, wait .3 seconds to recompile
-    poll: 500 // Check for edits every 5 seconds
-  },
-  plugins: [
-    new FriendlyErrorsWebpackPlugin(),
-    new webpack.ProgressPlugin(),
-    new HtmlWebpackPlugin({
-      template: path.resolve(process.cwd(), "public", "index.html")
-    })
-  ]
+    mode: "development",
+    devtool: "cheap-module-eval-source-map",
+    entry: {
+        main: path.resolve(process.cwd(), "src", "main.js")
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [
+                'style-loader',
+                'css-loader'
+            ]
+        }]
+    },
+    output: {
+        path: path.resolve(process.cwd(), "docs"),
+        publicPath: ""
+    },
+    node: {
+        fs: "empty",
+        net: "empty"
+    },
+    watchOptions: {
+        // ignored: /node_modules/,
+        aggregateTimeout: 300, // After seeing an edit, wait .3 seconds to recompile
+        poll: 500 // Check for edits every 5 seconds
+    },
+    plugins: [
+        new FriendlyErrorsWebpackPlugin(),
+        new webpack.ProgressPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.resolve(process.cwd(), "public", "index.html")
+        })
+    ]
 }
